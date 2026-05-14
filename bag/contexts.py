05ec66bd11ac3912e5a0873955f48dad1c72d088
Jamie12341324 <peacfulmarinecat@gmail.com>
2026-05-14 12:11:04 +0100

    Fix typo

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,7 +12,7 @@ def bag_contents(request):
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery=0
-        free=free_delivery_delta=0
+        free_delivery_delta=0
     grand_total = delivery + total
     context = {
         'bag_items': bag_items,
