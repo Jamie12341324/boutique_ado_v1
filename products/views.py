db7105363466f5596e6f815d143ba394cd41100a
Jamie12341324 <peacfulmarinecat@gmail.com>
2026-05-28 15:50:53 +0100

    Product Admin - Editing Products

--- products/views.py
+++ products/views.py
@@ -68,10 +68,30 @@ def add_product(request):
             messages.error(request, 'Failed to add product. Please ensure form is valid.')
     else:
         form=ProductForm()
-        
+
     form = ProductForm()
     template ='products/add_products.html'
     context = {
         'form': form,
+    }
+    return render(request, template, context)
+
+def edit_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method =='POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product_id]))
+        else:
+            messages.error(request, f'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'you are editing {product.name}')
+    template ='products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
     return render(request, template, context)
