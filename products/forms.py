6180b7aca996a1969bcf9c9948e31b75cc0c512f
Jamie12341324 <peacfulmarinecat@gmail.com>
2026-05-28 10:12:24 +0100

    Product Admin - add product view, url and template

--- products/forms.py
+++ products/forms.py
@@ -2,16 +2,16 @@ from django import forms
 from .models import Product, Category
 
 
-
 class ProductForm(forms.ModelForm):
 
-    class meta:
+    class Meta:
         model = Product
         fields = '__all__'
+
     def __init__(self, *args, **kwargs):
-        super().__init(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_names = [(c.id, c.get, friendly_name()) for c in categories]
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
+    path('<product_id>/', views.product_detail, name='product_detail'),
+    
 ]
